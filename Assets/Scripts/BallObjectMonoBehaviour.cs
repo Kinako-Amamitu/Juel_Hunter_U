2f6db75973d674747e3779fc08bff3f74512989e
S-JOBI23-060\student <231122@st.yoshida-g.ac.jp>
2024-07-18 09:15:08 +0900

    a

---
+++ Assets/Scripts/BallObjectMonoBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallObject : MonoBehaviour
+{
+    [SerializeField]
+    public new Renderer renderer;
+
+    [SerializeField]
+    public bool isTouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        //ボールにぶつかったとき
+        if (other.gameObject.tag == "Ball")
+        {
+            Debug.Log("ボールにぶつかった！");
+        }
+        else
+        {
+            Debug.Log("ボールじゃないところにぶつかった！");
+            Destroy(gameObject);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    public float fMoveSpeed = 7.0f;     // 移動値
+    //---------------------------
+    //          追加
+    public GameObject BulletObj;        // 弾のゲームオブジェクト
+    //---------------------------
+    Vector3 bulletPoint;                // 弾の位置
+
+    void Start()
+    {
+        bulletPoint = transform.Find("BulletPoint").localPosition;
+    }
+    // Update is called once per frame
+    void Update()
+	{
+        // ボタンを押したとき
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // 弾の生成
+            Instantiate(BulletObj, transform.position + bulletPoint, Quaternion.identity);
+        }
+    }
+}
