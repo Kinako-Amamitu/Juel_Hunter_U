f1037c32da1b56fb297d89a33b3066ffd47a2df0
S-JOBI23-060\student <231122@st.yoshida-g.ac.jp>
2024-09-20 17:23:01 +0900

    バグ修正

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -7,9 +7,15 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] Vector2 velocity;
 
+    public AudioClip ninja; //死亡音
+    AudioSource audioSource; //SE入力にオーディオソースを使用する
+
     private void Start()
     {
         GetComponent<Rigidbody2D>().velocity = velocity;
+
+        //AudioComponentを取得
+        audioSource = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,12 +24,13 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
         }
         else if (collision.gameObject.tag == "Blue"||collision.gameObject.tag=="Red"
-            ||collision.gameObject.tag=="Green"||collision.gameObject.tag=="Pink"||collision.gameObject.tag=="Yellow")
+            ||collision.gameObject.tag=="Green"||collision.gameObject.tag=="Purple"||collision.gameObject.tag=="Yellow")
         {
+           
+           
             Destroy(gameObject);
             Destroy(collision.gameObject);
         }
 
     }
-   
 }
